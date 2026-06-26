9a380d4f32e6a7af9531472358cfcd9d445a0941
Hai Pham Ngoc <ngochai285nd@gmail.com>
2026-06-26 22:41:27 +0700

    chore: configure vite and tauri, fix negated config condition

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,11 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
-// https://vite.dev/config/
-export default defineConfig(async () => ({
-  plugins: [react()],
-
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent Vite from obscuring rust errors
+export default defineConfig({
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
-    port: 1420,
+    port: 5173,
     strictPort: true,
     host: host || false,
     hmr: host
@@ -24,9 +15,16 @@ export default defineConfig(async () => ({
           port: 1421,
         }
       : undefined,
+
     watch: {
-      // 3. tell Vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
     },
   },
-}));
+  envPrefix: ["VITE_", "TAURI_ENV_*"],
+  build: {
+    target:
+      process.env.TAURI_ENV_PLATFORM == "windows" ? "chrome105" : "safari13",
+    minify: process.env.TAURI_ENV_DEBUG ? false : "esbuild",
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+  },
+});
