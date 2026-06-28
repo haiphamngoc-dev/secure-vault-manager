5dd6af97a5d7604bb28470bd7e670ca865ff0d93
Hai Pham Ngoc <ngochai285nd@gmail.com>
2026-06-28 11:54:49 +0700

    fix(root): configure WebAssembly support plugins for Vite production builds

--- packages/desktop/vite.config.ts
+++ packages/desktop/vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "vite";
 import path from "node:path";
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 const host = process.env.TAURI_DEV_HOST;
 
 export default defineConfig({
+  plugins: [wasm(), topLevelAwait()],
   clearScreen: false,
   resolve: {
     alias: {
@@ -29,7 +32,7 @@ export default defineConfig({
   envPrefix: ["VITE_", "TAURI_ENV_*"],
   build: {
     target:
-      process.env.TAURI_ENV_PLATFORM == "windows" ? "chrome105" : "safari13",
+      process.env.TAURI_ENV_PLATFORM == "windows" ? "chrome105" : "safari15",
     minify: process.env.TAURI_ENV_DEBUG ? false : "esbuild",
     sourcemap: !!process.env.TAURI_ENV_DEBUG,
   },
--- packages/extension/vite.config.ts
+++ packages/extension/vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), wasm(), topLevelAwait()],
   build: {
+    target: "esnext",
     rollupOptions: {
       input: {
         popup: path.resolve(__dirname, "popup.html"),
